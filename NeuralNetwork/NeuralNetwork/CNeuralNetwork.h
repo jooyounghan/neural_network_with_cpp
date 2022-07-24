4a448325de7ceee40b5c620613be8ab41e8ca25b
jooyounghan <rsfnqf98@naver.com>
2022-07-24 09:22:29 +0900

    20220724

--- NeuralNetwork/NeuralNetwork/CNeuralNetwork.h
+++ NeuralNetwork/NeuralNetwork/CNeuralNetwork.h
@@ -43,6 +43,8 @@ protected:
 	bool CompareInputProperties(std::shared_ptr<CMatrix> input);
 
 public:
+	// TODO : INPUT / LABEL 과 관련된 별도의 클래스를 생성하여
+	// shared_ptr를 내부적으로 숨겨야 함.
 	CMatrix Predict(std::shared_ptr<CMatrix> input);
 	double Train(std::shared_ptr<CMatrix> input, std::shared_ptr<CMatrix> label);
 };
